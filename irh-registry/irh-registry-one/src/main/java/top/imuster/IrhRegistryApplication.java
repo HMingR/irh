2662d94d1f57637934573a88f68c41f1750aecdd
李鹏飞 <1363618637@qq.com>
2019-11-24 20:14:29 +0800

    增加了注册中心

---
+++ irh-registry/irh-registry-one/src/main/java/top/imuster/IrhRegistryApplication.java
@@ -0,0 +1,13 @@
+package top.imuster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class IrhRegistryApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(IrhRegistryApplication.class);
+    }
+}
