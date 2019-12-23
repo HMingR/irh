c01f6a7648b06e40c1d6ec975c52026a4e86944b
李鹏飞 <1363618637@qq.com>
2019-12-23 09:46:15 +0800

    修改了文件上传微服务

--- irh-service/irh-service-goods/src/main/java/top/imuster/goods/controller/ProductController.java
+++ irh-service/irh-service-goods/src/main/java/top/imuster/goods/controller/ProductController.java
@@ -1,10 +1,11 @@
 package top.imuster.goods.controller;
 
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import top.imuster.common.base.controller.BaseController;
+import top.imuster.common.base.wrapper.Message;
 
 /**
  * @ClassName: ProductController
@@ -12,14 +13,14 @@ import top.imuster.common.base.controller.BaseController;
  * @author: hmr
  * @date: 2019/12/1 14:53
  */
-@Controller
+@RestController
 @RequestMapping("product")
 public class ProductController extends BaseController {
 
     @GetMapping("test")
-    public String test() {
-
-        return "test";
+    public Message<String> test() {
+        System.out.println("testing................");
+        return Message.createBySuccess("测试Product成功", "测试数据");
     }
 
 }
